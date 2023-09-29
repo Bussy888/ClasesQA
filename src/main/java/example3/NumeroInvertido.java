be681bec56c93f84c37c152ff971626d64025c05
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2023-09-28 20:21:58 -0400

    Tarea de numero invertido

--- src/main/java/example3/NumeroInvertido.java
+++ src/main/java/example3/NumeroInvertido.java
@@ -1,2 +1,20 @@
-package example3;public class NumeroInvertido {
+package example3;
+
+public class NumeroInvertido {
+    public String invertir(int numero) {
+        if (numero < 0) {
+            return "Valor Incorrecto";
+        } else {
+            String cadena = String.valueOf(numero);
+            if (cadena.length() == 1) {
+                return cadena;
+            } else {
+                StringBuilder invertido = new StringBuilder();
+                for (int i = cadena.length() - 1; i >= 0; i--) {
+                    invertido.append(cadena.charAt(i));
+                }
+                return invertido.toString();
+            }
+        }
+    }
 }
